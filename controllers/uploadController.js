92b49d768a5cb9186e791f91f67f4dab51b1f07c
alikahwaji <ali.kahwaji@nmit.ac.nz>
2024-03-01 01:31:31 +0000

    Completed the upload controller

---
+++ controllers/uploadController.js
@@ -0,0 +1,16 @@
+const { STATUS_CODES } = require('http')
+const multer = require('multer')
+const path = require('path')
+const upload = multer({ dest: path.join(__dirname, '../uploads')})
+
+const uploadFile = (req,res) => {
+    upload.single('file')(req, res, function (err) {
+        if(err) {
+            return res.status(500).json({message: 'File upload failed', error: err})
+        }
+        // Process the file
+        return res.status(200).json({ message: 'File uploaded successfully'})
+    })
+}
+
+module.exports = {uploadFile}
