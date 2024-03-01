9a4b02cde092879b226ee4359a6e1b51cee087c1
alikahwaji <ali.kahwaji@nmit.ac.nz>
2024-03-01 00:21:03 +0000

    Server app done

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express = require ('express')
 const excellRoutes = require ('./routes/excelRoutes')
 const app = express()
+const port = 3000
+
+app.use('/api/excel', excellRoutes)
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
+
+module.exports = app
